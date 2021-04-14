b46c94583e9af6ac549c62f1f7571e1d2a4d2758
tufangorel <tufang@gmail.com>
2021-04-14 15:18:38 +0300

    service method @Transactional( transactionManager = "customerTransactionManager", isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED) annotation added.

--- src/main/java/com/company/customerinfo/service/CustomerService.java
+++ src/main/java/com/company/customerinfo/service/CustomerService.java
@@ -5,6 +5,9 @@ import com.company.customerinfo.model.Customer;
 import com.company.customerinfo.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +17,7 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Transactional( transactionManager = "customerTransactionManager", isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
     public Customer save(Customer customer){
         return customerRepository.save(customer);
     }
